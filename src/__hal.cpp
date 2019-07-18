b9948cbedda79aa653d2fcfdb0d4e2e7ebb9de0c
unknown <voicevon@gmail.com>
2019-07-18 12:57:33 +0800

    tested cpp

--- src/__hal.cpp
+++ src/__hal.cpp
@@ -1,1 +1,7 @@
 # include "__hal.h"
+
+// board  https://github.com/Smoothieware/SmoothieV2/blob/ed7237065621f55f37fd420ea0b78b91bc411d63/Firmware/Hal/lpc_board/src/board.c
+void Board_Init(void){
+
+}
+
--- src/__hal.h
+++ src/__hal.h
@@ -27,22 +27,22 @@ typedef void * TaskHandle_t;
 #define xTaskHandle TaskHandle_t
 //uart  //https://github.com/Smoothieware/SmoothieV2/blob/97f5278ae5f26b751c71a98927c8c539ff78a2e8/Firmware/Hal/src/uart_comms.h
 int setup_uart(){return 1;}
-size_t read_uart(char * buf, size_t length){return 1;}
+size_t read_uart(char * buf, size_t length);
 size_t write_uart(const char * buf, size_t length){return 1;}
-void set_notification_uart(xTaskHandle h){}
+void set_notification_uart(xTaskHandle h);
 
 
 // board  https://github.com/Smoothieware/SmoothieV2/blob/ed7237065621f55f37fd420ea0b78b91bc411d63/Firmware/Hal/lpc_board/src/board.c
-void Board_Init(void){}
-void Board_LED_Toggle(uint8_t LEDNumber){}
-void Board_LED_Set(uint8_t LEDNumber, bool On){}
+void Board_Init(void);
+void Board_LED_Toggle(uint8_t LEDNumber);
+void Board_LED_Set(uint8_t LEDNumber, bool On);
 
 // cpu    https://github.com/Smoothieware/SmoothieV2/blob/fc46e2b88e431686f5a4f83e04e2e2c58fa20692/Firmware/Hal/lpc_chip_43xx/inc/core_cm3.h
-void NVIC_SetPriorityGrouping(uint32_t PriorityGroup){}
+void NVIC_SetPriorityGrouping(uint32_t PriorityGroup);
 //    cpu https://github.com/Smoothieware/SmoothieV2/blob/7c65922bb1ddc79103430f9747af1c5fa24170ba/flashloader/lpc_chip_43xx/src/chip_18xx_43xx.c
 uint32_t SystemCoreClock;
-void SystemCoreClockUpdate(void){}
+void SystemCoreClockUpdate(void);
 
 // stopwatch  https://github.com/Smoothieware/SmoothieV2/blob/46fa7b9afa093e504639172c06b06aff42bd0f36/Firmware/Hal/lpc_chip_43xx/inc/stopwatch.h
-void StopWatch_Init(void){}
+void StopWatch_Init(void);
 uint32_t StopWatch_TicksPerSecond(void){return 1;}
