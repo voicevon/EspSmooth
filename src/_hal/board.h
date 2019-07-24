9381630f6d69395af7cfea7dbf48832d1a3f01d4
unknown <voicevon@gmail.com>
2019-07-24 20:01:50 +0800

    sd cand and file system to new hal

--- src/_hal/board.h
+++ src/_hal/board.h
@@ -3,6 +3,9 @@
 #define _HAL_BOARD_H_
 
 #include "stdint.h"
+#include "stdbool.h"
+
+
 // board  https://github.com/Smoothieware/SmoothieV2/blob/ed7237065621f55f37fd420ea0b78b91bc411d63/Firmware/Hal/lpc_board/src/board.c
 void Board_Init(void);
 void Board_LED_Toggle(uint8_t LEDNumber);
