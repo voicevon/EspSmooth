f1a415634b812f0b961dd94b07af00159e1f34ec
unknown <voicevon@gmail.com>
2019-07-30 18:13:52 +0800

    simplest servermotor should be ok

--- src/robot/ServoMotor.cpp
+++ src/robot/ServoMotor.cpp
@@ -7,15 +7,15 @@
 
 // The following array and two functions are needed because the analogWrite() function
 // is not implemented for the ESP32.
+
 #include "esp32-hal-ledc.h"
 #include "esp32-hal-adc.h"
 #include "ServoMotor.h"
 
 
-
 // The default values freq=500 and resolution=8 are used for compatibility with the
 // Arduino UNO, which generates PWM signals at 500Hz and 8 bit resolution.
-void ServoMotor::setupAnalogWritePin(int pin, int channel, int freq, int resolution)
+void ServoMotor::__setupAnalogWritePin(int pin, int channel, int freq, int resolution)
 {
   ledcSetup(channel, freq, resolution);
   __pin2channel[pin] = channel;
@@ -28,21 +28,22 @@ void ServoMotor::analogWriteESP32(int pin, int value)
   ledcWrite(__pin2channel[pin], value);
 }
 
-const int SERVO = 18;         // Pin to control (pulse) servo
+// const int SERVO = 18;         // Pin to control (pulse) servo
 const int SERVO_CHANNEL = 0;  // 0-15
 const int SERVO_FREQ = 50;    // 50Hz
 const int SERVO_RES = 16;     // 16 bit resolution
 
-
 const int MIN_PULSE = 1575; // 0.48mS previously 0xFFFF/20 for 1.0mS
 const int MAX_PULSE = 7335; // 2.24mS previously 0xFFFF/10 for 2.0mS
 
-int val = 0; // For storing the reading from the POT
 
-void ServoMotor::setup_ledc()
+void ServoMotor::setup_ledc(int gpio_pin_number)
 {
-  setupAnalogWritePin(SERVO, SERVO_CHANNEL, SERVO_FREQ, SERVO_RES);
+    __setupAnalogWritePin(gpio_pin_number, SERVO_CHANNEL, SERVO_FREQ, SERVO_RES);
+    __gpio_pin_number = gpio_pin_number;
 }
+
+
 
 long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
@@ -53,9 +54,10 @@ const int POT   = 34; // Pin to middle tap of pot
 
 void ServoMotor::__test()
 {
+  int val = 0; // For storing the reading from the POT
   val = analogRead(POT); // ESP32's A/D varies from 0-4095 (12 bits) 
                          //              instead of 0-1023 (10 bits).
 
   val = map(val, 0, 4095, MIN_PULSE, MAX_PULSE); // Scale A/D value to servo range
-  analogWriteESP32(SERVO, val); // Set the servo
+  analogWriteESP32(__gpio_pin_number, val); // Set the servo
 }
--- src/robot/ServoMotor.h
+++ src/robot/ServoMotor.h
@@ -5,12 +5,13 @@
 
 class ServoMotor{
 public:
-    void setupAnalogWritePin(int pin, int channel, int freq=500, int resolution=8);
     void analogWriteESP32(int pin, int value);
-    void setup_ledc();
+    void setup_ledc(int gpio_pin_number);
 private:
     void __test();
     int __pin2channel[64]; // holds the PWM channel (0-15) attached to a given pin (0-63)
+    void __setupAnalogWritePin(int pin, int channel, int freq=500, int resolution=8);
+    int __gpio_pin_number;
 };
 
 
