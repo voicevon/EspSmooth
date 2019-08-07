1ce1ca8fb4429916cb9232c6038e24d4ad90e77c
unknown <voicevon@gmail.com>
2019-08-07 08:24:47 +0800

    add mqtt

---
+++ src/esphome/components/ram_sensor/ram_sensor_int16.cpp
@@ -0,0 +1,32 @@
+
+#include "ram_sensor_int16.h"
+#include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
+#include "esphome.h"
+
+
+
+namespace esphome {
+namespace ramsensor {
+
+
+static const char *TAG = "ram_sensor_int16";
+
+void RamSensorInt16::setup(){
+    this->state = 0;
+    ESP_LOGCONFIG(TAG, "Setting up INT_SENSOR '%s'...", this->name_.c_str());
+}
+
+void RamSensorInt16::set_int_source(int16_t* target){
+  this->pInt16 = target;
+}
+
+void RamSensorInt16::loop(){
+  if (int16_t(this->state) != *this->pInt16){
+    this->state = *this->pInt16;
+    this->publish_state(this->state);
+  }
+}
+
+}  // namespace intsensor
+}  // namespace esphome
