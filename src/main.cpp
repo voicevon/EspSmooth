7c92e8bf2d072487197f069aa4d10d26038a0c28
unknown <voicevon@gmail.com>
2019-08-10 00:45:59 +0800

    test again

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,74 @@
-#include "HardwareSerial.h"
-#include "SPIFFS.h"     // ESP class
+// #include "HardwareSerial.h"
+// #include "SPIFFS.h"     // ESP class
 
-#include "_hal/board.h"
-#include "_hal/stopwatch.h"
-#include "smoothie/robot/Actuator/ActuatorTask.h"
-#include "smoothie/RobotStarter.h"
-#include "main.h"
-
-
-static const char *TAG = "espsmooth.main";
+// #include "_hal/board.h"
+// #include "_hal/stopwatch.h"
+// #include "smoothie/robot/Actuator/ActuatorTask.h"
+// #include "smoothie/RobotStarter.h"
+// #include "main.h"
 
 
-// extern void configureSPIFI();
-// extern void smoothie_startup(void *);
+// static const char *TAG = "espsmooth.main";
 
 
-// void smooth_setup(){
+// // extern void configureSPIFI();
+// // extern void smoothie_startup(void *);
 
-//     StopWatch_Init();
-//     printf("StopWatch clock rate= %lu Hz\n", StopWatch_TicksPerSecond());
 
-//     // launch the startup thread which will become the command thread that executes all incoming commands
-//     // 10000 Bytes stack
-//     xTaskCreate(smoothie_startup, "CommandThread", 30000, NULL, (tskIDLE_PRIORITY + 2UL), (TaskHandle_t *) NULL);
+// // void smooth_setup(){
+
+// //     StopWatch_Init();
+// //     printf("StopWatch clock rate= %lu Hz\n", StopWatch_TicksPerSecond());
+
+// //     // launch the startup thread which will become the command thread that executes all incoming commands
+// //     // 10000 Bytes stack
+// //     xTaskCreate(smoothie_startup, "CommandThread", 30000, NULL, (tskIDLE_PRIORITY + 2UL), (TaskHandle_t *) NULL);
     
+// // }
+
+// extern float float_value;
+
+// void setup(){
+//     Board_report_memory();
+//     Board_Init();
+//     esphome_setup();   //wifi setup must be in advance of starting a timer_interrupt. Even RTOS. 
+//     Board_report_memory();
+
+//     smoothie_setup(); 
+//     delay(5000);   //Keep uartTx empty for ProntFace handshaking.
+//     printf("\n\n"); 
+//     printf("Hi, Mr.ProntFace. You're online now. right?\n ");
+//     Controlmotors_setup();
+//     Board_report_cpu();
+//     Board_report_memory();
 // }
 
-extern float float_value;
+// #include "smoothie/robot/Robot.h"
+// #include "smoothie/robot/Actuator/DcMotor.h"
 
-void setup(){
-    Board_report_memory();
-    Board_Init();
-    esphome_setup();   //wifi setup must be in advance of starting a timer_interrupt. Even RTOS. 
-    Board_report_memory();
+// uint64_t rtos_report_inteval_second = 5 ;
+// uint64_t cpu_idle_counter = 0;
+// uint64_t last_time_stamp = 0;   //us
+// // Actually, this is the lowest priority task.
+// void loop(){
+//     cpu_idle_counter++;
+//     if(esp_timer_get_time () - last_time_stamp >= rtos_report_inteval_second * 1000000){
+//         uint16_t passed_time = cpu_idle_counter / 10280 / rtos_report_inteval_second;
+//         uint16_t uptime_second = esp_timer_get_time() / 1000000;  
+//         printf("uptime = %i seconds, cpu usage =  %i/%%  ",uptime_second, 100 - passed_time);
 
-    smoothie_setup(); 
-    delay(5000);   //Keep uartTx empty for ProntFace handshaking.
-    printf("\n\n"); 
-    printf("Hi, Mr.ProntFace. You're online now. right?\n ");
-    Controlmotors_setup();
-    Board_report_cpu();
-    Board_report_memory();
-}
+//         //vTaskList(ptrTaskList);   vTaskList is not supportted?  Jun2019      https://github.com/espressif/esp-idf/issues/416
 
-#include "smoothie/robot/Robot.h"
-#include "smoothie/robot/Actuator/DcMotor.h"
+//         cpu_idle_counter = 0;
+//         last_time_stamp = esp_timer_get_time();
 
-uint64_t rtos_report_inteval_second = 5 ;
-uint64_t cpu_idle_counter = 0;
-uint64_t last_time_stamp = 0;   //us
-// Actually, this is the lowest priority task.
-void loop(){
-    cpu_idle_counter++;
-    if(esp_timer_get_time () - last_time_stamp >= rtos_report_inteval_second * 1000000){
-        uint16_t passed_time = cpu_idle_counter / 10280 / rtos_report_inteval_second;
-        uint16_t uptime_second = esp_timer_get_time() / 1000000;  
-        printf("uptime = %i seconds, cpu usage =  %i/%%  ",uptime_second, 100 - passed_time);
-
-        //vTaskList(ptrTaskList);   vTaskList is not supportted?  Jun2019      https://github.com/espressif/esp-idf/issues/416
-
-        cpu_idle_counter = 0;
-        last_time_stamp = esp_timer_get_time();
-
-        DcMotor* dc =(DcMotor*) Robot::getInstance()->actuators[1];
-        // float dc_angle = dc->for_test_read_sensor_position();
-        float dc_angle= dc->get_current_position();
-        printf("    Y Pos= %f", dc_angle);
-        printf("\n");
+//         DcMotor* dc =(DcMotor*) Robot::getInstance()->actuators[1];
+//         // float dc_angle = dc->for_test_read_sensor_position();
+//         float dc_angle= dc->get_current_position();
+//         printf("    Y Pos= %f", dc_angle);
+//         printf("\n");
         
-    }
+//     }
 
-}
+// }
 
