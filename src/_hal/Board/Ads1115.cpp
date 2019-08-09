a5b6e52481a2e446dfc9ed1c0bdcd9484c75b639
unknown <voicevon@gmail.com>
2019-08-10 01:14:12 +0800

    individual channel

--- src/_hal/Board/Ads1115.cpp
+++ src/_hal/Board/Ads1115.cpp
@@ -28,12 +28,15 @@ void setup_ads1115(ConfigReader cr){
     for(int j=0; j<CHANNEL_COUNT; j++){
       ads1115_ads1115sensor[i][j] = new ads1115::ADS1115Sensor(ads1115_ads1115component[i]);
       ads1115_ads1115component[i]->register_sensor(ads1115_ads1115sensor[i][j]);
+      if(j==0) ads1115_ads1115sensor[i][j]->set_multiplexer(ads1115::ADS1115_MULTIPLEXER_P0_NG); 
+      if(j==1) ads1115_ads1115sensor[i][j]->set_multiplexer(ads1115::ADS1115_MULTIPLEXER_P1_NG); 
+      if(j==2) ads1115_ads1115sensor[i][j]->set_multiplexer(ads1115::ADS1115_MULTIPLEXER_P2_NG); 
+      if(j==3) ads1115_ads1115sensor[i][j]->set_multiplexer(ads1115::ADS1115_MULTIPLEXER_P3_NG); 
       // ads1115_ads1115sensor[0][0]->set_name("alpha_pos");
       // ads1115_ads1115sensor[0][0]->set_unit_of_measurement("");
       // ads1115_ads1115sensor[0][0]->set_icon("");
       // ads1115_ads1115sensor[0][0]->set_accuracy_decimals(3);
       // ads1115_ads1115sensor[0][0]->set_update_interval(1000);  //from component? or pulling_sensor?
-      ads1115_ads1115sensor[i][i]->set_multiplexer(ads1115::ADS1115_MULTIPLEXER_P1_NG); 
       ads1115_ads1115sensor[i][j]->set_gain(ads1115::ADS1115_GAIN_6P144);               
       ads1115_ads1115sensor[i][j]->setup();
     }
