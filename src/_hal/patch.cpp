503381066409943d5c3f9427823a11d6c7816744
unknown <voicevon@gmail.com>
2019-08-12 19:38:38 +0800

    remove rubish code

--- src/_hal/patch.cpp
+++ src/_hal/patch.cpp
@@ -2,12 +2,12 @@
 
 namespace patch
 {
-    template < typename T > std::string to_string( const T& n )
-    {
-        std::ostringstream stm ;
-        stm << n ;
-        return stm.str() ;
-    }
+    // template < typename T > std::string to_string( const T& n )
+    // {
+    //     std::ostringstream stm ;
+    //     stm << n ;
+    //     return stm.str() ;
+    // }
 
     uint32_t min(uint32_t x, uint32_t y){
         if(x < y) return x;
@@ -16,9 +16,9 @@ namespace patch
 
 }
 
-int strncmp ( const char * str1, const char * str2, size_t num ){
-    return 0;
-}
+// int strncmp ( const char * str1, const char * str2, size_t num ){
+//     return 0;
+// }
 
 void * memset ( void * ptr, int value, size_t num ){
     void * xx;
