72ebf747adb421aacbe37c595d23b4bf6b810c5f
unknown <voicevon@gmail.com>
2019-11-19 10:18:35 +0800

    optimaized

--- src/Apps/Weeder/OneSide.cpp
+++ src/Apps/Weeder/OneSide.cpp
@@ -23,18 +23,18 @@ float OneSide::__get_error_length_of_actuaotr(){
     float target = __convert_command_from_toucher();
     return target - actuator_feedback.get_meaning_value();
 }
-void OneSide::pid_loop_with_reading_sensors(){
-    read_sensors();
-    float target_length = __convert_command_from_toucher();
-    __error_length =  actuator_feedback.get_meaning_value() - target_length ;
 
+void OneSide::set_show_debug(bool config){
+    __config_show_debug = config;
+}
+void OneSide::__show_debug(){
     // Serial.print("read_sensors()  toucher adc = ");
     // Serial.print(toucher.get_last_reading());
     Serial.print("read_sensors()  toucher length = ");
     Serial.print(toucher.get_meaning_value());
 
     Serial.print ("  target_length = ");
-    Serial.print (target_length);
+    Serial.print (__target_length);
 
     // Serial.print("actuator feedback adc = ");
     // Serial.print(actuator_feedback.get_last_reading());
@@ -43,28 +43,38 @@ void OneSide::pid_loop_with_reading_sensors(){
     Serial.print("  error_length = ");
     Serial.print(__error_length);
     Serial.println(" ");
+}
+
+void OneSide::pid_loop_with_reading_sensors(){
+    read_sensors();
+    __target_length = __convert_command_from_toucher();
+    __error_length =  actuator_feedback.get_meaning_value() - __target_length ;
+
+    if(__config_show_debug) __show_debug();
     // return;
 
     // pid_loop
-    if(__error_length > 68) __error_length = 68;
-    if(__error_length < -68) __error_length = -68;
+    if(__error_length > __error_max) __error_length = __error_max;
+    if(__error_length < __error_min) __error_length = __error_min;
 
     pid_controller.UpdateError(__error_length);
     float output = pid_controller.get_output();
     // pid_controller.show_errors_and_output();
 
-    // output pwm
-    //uint16_t pwm_output = toucher.get_meaning_value() * 65536.0 / 61.0  - 177.0f * 65536.0f / 61.0f  ;
-    //uint16_t pwm_output = - output * 32768 / 300 + 32768 ;   // 4mA == Shorter length
+    // output pwm.
     uint16_t pwm_output = output * 32768 / 150 + 32768 ;   // 20mA == Longer length
     pwm_output = 65536 - pwm_output;
-    bool endstop = false;
-    if (actuator_feedback.get_meaning_value() < 420  && target_length < 410)
+
+    // To protect actuaotr. 
+    // When actuator reaches/close to end_point, but the commander want to move outside more,
+    // in this scenario, we should stop actuator to move further. 
+    bool endstop = false;  
+    if (actuator_feedback.get_meaning_value() < 420  && __target_length < 410)
         endstop = true;
-    if ( actuator_feedback.get_meaning_value() > 580 && target_length > 580)
+    if ( actuator_feedback.get_meaning_value() > 580 && __target_length > 580)
         endstop = true;
     if (endstop)
-        pwm_output = 32768;
+        pwm_output = 32768;   //output 12mA, means stop actuator.
 
     // Serial.print("PWM output = ");
     // Serial.println(pwm_output);
--- src/Apps/Weeder/OneSide.h
+++ src/Apps/Weeder/OneSide.h
@@ -16,14 +16,20 @@ class OneSide{
         ~OneSide();
         void init_actuator(PwmPin actuator_pin);
         void pid_loop_with_reading_sensors();   
+        void set_show_debug(bool config);
         void read_sensors();
         float test_float;
         PID pid_controller; 
 
     private:
         float __error_length;
+        float __error_min = -68.0f;
+        float __error_max = 68.0f;
+        bool __config_show_debug = false;
         float __get_error_length_of_actuaotr();
         float __convert_command_from_toucher();
+        void __show_debug();
+        float __target_length;
 };
 
 #endif
